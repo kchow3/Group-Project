b2f50d392ac8300e36d8d4dfa773a2e92898fbdd
kchow3 <kchow3@ualberta.ca>
2014-03-02 10:49:35 -0700

    Comment Class added attribute variables.

--- src/cs/ualberta/ca/tunein/Comment.java
+++ src/cs/ualberta/ca/tunein/Comment.java
@@ -1,9 +1,28 @@
 package cs.ualberta.ca.tunein;
 
-public class Comment {
+import java.util.ArrayList;
+import java.util.Date;
 
-	public Comment() {
-		// TODO Auto-generated constructor stub
+public class Comment {
+	
+	private Commenter commenter;
+	private String title;
+	private String comment;
+	private ArrayList<Comment> replies;
+	private Image img;
+	private Date date;
+	private GeoLocation geolocation;
+	
+
+	public Comment(Commenter user, String aTitle, String aComment, Image aImage, GeoLocation loc) 
+	{
+		
+		this.commenter = user;
+		this.title = aTitle;
+		this.comment = aComment;
+		this.img = aImage;
+		this.date = new Date();
+		this.geolocation = loc;
 	}
 
 }
--- src/cs/ualberta/ca/tunein/Commentator.java
+++
@@ -1,9 +0,0 @@
-package cs.ualberta.ca.tunein;
-
-public class Commentator {
-
-	public Commentator() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
