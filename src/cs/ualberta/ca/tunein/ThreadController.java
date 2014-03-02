44ae8eca4f0dd51f2c67dbdf84610d577c1d1ec4
kchow3 <kchow3@ualberta.ca>
2014-03-02 14:41:42 -0700

    Added code to classes that are associated with the thread controller classes.

--- src/cs/ualberta/ca/tunein/ThreadController.java
+++ src/cs/ualberta/ca/tunein/ThreadController.java
@@ -1,9 +1,38 @@
 package cs.ualberta.ca.tunein;
 
+import java.util.ArrayList;
+
 public class ThreadController implements ThreadControllerInterface {
 
+	private Thread discussionThread;
+	
 	public ThreadController() {
-		// TODO Auto-generated constructor stub
+	
+		discussionThread = new Thread();
+	}
+
+	@Override
+	public void sortByLocation(GeoLocation loc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortBySetLocation(GeoLocation loc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortByPicture() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortByDate() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/cs/ualberta/ca/tunein/ThreadControllerInterface.java
+++ src/cs/ualberta/ca/tunein/ThreadControllerInterface.java
@@ -1,5 +1,9 @@
 package cs.ualberta.ca.tunein;
 
 public interface ThreadControllerInterface {
-
+	
+	public void sortByLocation(GeoLocation loc);
+	public void sortBySetLocation(GeoLocation loc);
+	public void sortByPicture();
+	public void sortByDate();
 }
