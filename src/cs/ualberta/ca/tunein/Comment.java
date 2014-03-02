e9c1cd5d8fa0446f7d101fc69dca2d0791c69630
kchow3 <kchow3@ualberta.ca>
2014-03-02 11:56:55 -0700

    Added code to the comment class and the discussion thread class that contains an array list.

--- src/cs/ualberta/ca/tunein/Comment.java
+++ src/cs/ualberta/ca/tunein/Comment.java
@@ -12,17 +12,118 @@ public class Comment {
 	private Image img;
 	private Date date;
 	private GeoLocation geolocation;
+	private boolean hasImage;
 	
-
-	public Comment(Commenter user, String aTitle, String aComment, Image aImage, GeoLocation loc) 
+	//constructor for creating a comment with no picture
+	public Comment(Commenter user, String aTitle, String aComment, GeoLocation loc) 
 	{
 		
 		this.commenter = user;
 		this.title = aTitle;
 		this.comment = aComment;
-		this.img = aImage;
-		this.date = new Date();
 		this.geolocation = loc;
+		this.date = new Date();
+		this.replies = new ArrayList<Comment>();
+		this.hasImage = false;
 	}
 
+	//constructor for creating comment with a picture
+	public Comment(Commenter user, String aTitle, String aComment, GeoLocation loc, Image aImage) 
+	{
+		
+		this.commenter = user;
+		this.title = aTitle;
+		this.comment = aComment;
+		this.geolocation = loc;
+		this.img = aImage;
+		this.date = new Date();
+		this.replies = new ArrayList<Comment>();
+		this.hasImage = true;
+	}
+
+
+	public Commenter getCommenter() 
+	{
+		return commenter;
+	}
+
+
+	public void setCommenter(Commenter commenter) 
+	{
+		this.commenter = commenter;
+	}
+
+
+	public String getTitle() 
+	{
+		return title;
+	}
+
+
+	public void setTitle(String title) 
+	{
+		this.title = title;
+	}
+
+
+	public String getComment() 
+	{
+		return comment;
+	}
+
+
+	public void setComment(String comment) 
+	{
+		this.comment = comment;
+	}
+
+
+	public ArrayList<Comment> getReplies() 
+	{
+		return replies;
+	}
+
+
+	public void setReplies(ArrayList<Comment> replies) 
+	{
+		this.replies = replies;
+	}
+
+
+	public Image getImg() 
+	{
+		return img;
+	}
+
+
+	public void setImg(Image img) 
+	{
+		this.img = img;
+	}
+
+
+	public Date getDate() 
+	{
+		return date;
+	}
+
+
+	public void setDate(Date date) 
+	{
+		this.date = date;
+	}
+
+
+	public GeoLocation getGeolocation() 
+	{
+		return geolocation;
+	}
+
+
+	public void setGeolocation(GeoLocation geolocation) 
+	{
+		this.geolocation = geolocation;
+	}
+
+	
 }
