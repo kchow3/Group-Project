91cc65a5d81e48e129c40c834ab171d9d4f6b77b
kchow3 <kchow3@ualberta.ca>
2014-03-04 11:23:28 -0700

    Added attributes to comment model.

--- src/cs/ualberta/ca/tunein/Comment.java
+++ src/cs/ualberta/ca/tunein/Comment.java
@@ -13,6 +13,10 @@ public class Comment {
 	private Date date;
 	private GeoLocation geolocation;
 	private boolean hasImage;
+	private boolean saved;
+	private boolean favorited;
+	private int favoriteCount;
+	private int replyCount;
 	
 	//constructor for creating a comment with no picture
 	public Comment(Commenter user, String aTitle, String aComment, GeoLocation loc) 
@@ -42,93 +46,126 @@ public class Comment {
 	}
 
 
-	public Commenter getCommenter() 
-	{
+	public Commenter getCommenter() {
 		return commenter;
 	}
 
 
-	public void setCommenter(Commenter commenter) 
-	{
+	public void setCommenter(Commenter commenter) {
 		this.commenter = commenter;
 	}
 
 
-	public String getTitle() 
-	{
+	public String getTitle() {
 		return title;
 	}
 
 
-	public void setTitle(String title) 
-	{
+	public void setTitle(String title) {
 		this.title = title;
 	}
 
 
-	public String getComment() 
-	{
+	public String getComment() {
 		return comment;
 	}
 
 
-	public void setComment(String comment) 
-	{
+	public void setComment(String comment) {
 		this.comment = comment;
 	}
 
 
-	public ArrayList<Comment> getReplies() 
-	{
+	public ArrayList<Comment> getReplies() {
 		return replies;
 	}
 
 
-	public void setReplies(ArrayList<Comment> replies) 
-	{
+	public void setReplies(ArrayList<Comment> replies) {
 		this.replies = replies;
 	}
 
 
-	public Image getImg() 
-	{
+	public Image getImg() {
 		return img;
 	}
 
 
-	public void setImg(Image img) 
-	{
+	public void setImg(Image img) {
 		this.img = img;
 	}
 
 
-	public Date getDate() 
-	{
+	public Date getDate() {
 		return date;
 	}
 
 
-	public void setDate(Date date) 
-	{
+	public void setDate(Date date) {
 		this.date = date;
 	}
 
 
-	public GeoLocation getGeolocation() 
-	{
+	public GeoLocation getGeolocation() {
 		return geolocation;
 	}
 
-
-	public void setGeolocation(GeoLocation geolocation) 
-	{
+	public void setGeolocation(GeoLocation geolocation) {
 		this.geolocation = geolocation;
 	}
 	
-	public void addReply(Comment aComment)
-	{
+	public void addReply(Comment aComment){
 		this.replies.add(aComment);
 	}
 
+	public boolean isHasImage() {
+		return hasImage;
+	}
+
+	public void setHasImage(boolean hasImage) {
+		this.hasImage = hasImage;
+	}
+
+	public boolean isSaved() {
+		return saved;
+	}
+
+	public void setSaved(boolean saved) {
+		this.saved = saved;
+	}
+
+	public boolean isFavorited() {
+		return favorited;
+	}
+
+	public void setFavorited(boolean favorited) {
+		this.favorited = favorited;
+	}
+
+	public int getFavoriteCount() {
+		return favoriteCount;
+	}
+
+	public void setFavoriteCount(int favoriteCount) {
+		this.favoriteCount = favoriteCount;
+	}
+
+	public int getReplyCount() {
+		return replyCount;
+	}
+
+	public void setReplyCount(int replyCount) {
+		this.replyCount = replyCount;
+	}
 	
+	public void increaseFavCount()
+	{
+		this.favoriteCount++;
+	}
+	
+	public void increaseReplyCount()
+	{
+		this.replyCount++;
+	}
+
 }
