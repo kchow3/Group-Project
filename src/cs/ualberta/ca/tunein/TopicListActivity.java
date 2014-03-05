9de03a0f2a92c7700ff1a54503ef42d3f70a99f5
kchow3 <kchow3@ualberta.ca>
2014-03-04 20:08:38 -0700

    Added code to the topic list activty class to open dialog box and pass information to activity. Added simple bitmap code to the image class.

--- src/cs/ualberta/ca/tunein/TopicListActivity.java
+++ src/cs/ualberta/ca/tunein/TopicListActivity.java
@@ -1,8 +1,16 @@
 package cs.ualberta.ca.tunein;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class TopicListActivity extends Activity {
 
@@ -10,6 +18,11 @@ public class TopicListActivity extends Activity {
 	private CommentViewAdapter viewAdapter;
 	//discussion thread list
 	private Thread threadList;
+	//variables for adding topic
+	private String title;
+	private String comment;
+	private Image img;
+	private boolean hasImg;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -31,6 +44,40 @@ public class TopicListActivity extends Activity {
 		//setup adapter
 		listview.setAdapter(viewAdapter);
 		
+	}
+	
+	public void createCommentClick(View v)
+	{
+		LayoutInflater inflater = LayoutInflater.from(TopicListActivity.this);
+		final View createView = inflater.inflate(R.layout.create_comment_view, null);
+
+		final TextView inputTitle = (EditText) createView.findViewById(R.id.textViewInputTitle);
+		final TextView inputComment = (EditText) createView.findViewById(R.id.editTextComment);
+		final ImageView inputImage = (ImageView) createView.findViewById(R.id.imageViewUpload);
+		
+		AlertDialog dialog = new AlertDialog.Builder(TopicListActivity.this)
+		    .setTitle("Create Comment")
+		    .setView(createView)
+		    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		        public void onClick(DialogInterface dialog, int whichButton) {
+		            title = inputTitle.getText().toString();
+		            comment = inputComment.getText().toString();
+		            
+		            if (inputImage.getVisibility() == View.VISIBLE) 
+		            {
+		            	inputImage.buildDrawingCache();
+		            	Bitmap bmp = inputImage.getDrawingCache();
+		            	//img = new Image(bmp);
+		            	hasImg = true;
+		            } 
+		            else 
+		            {
+		                hasImg = false;
+		            }
+		        }
+		    })
+		    .setNegativeButton("Cancel", null).create();
+		dialog.show();
 	}
 
 }
