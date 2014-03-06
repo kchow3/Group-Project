bfe8c77855a47672ae77059f7c65f8d68c4c6f3e
kchow3 <kchow3@ualberta.ca>
2014-03-06 13:38:37 -0700

    Added reply view adapter class.

--- src/cs/ualberta/ca/tunein/CommentPageActivity.java
+++ src/cs/ualberta/ca/tunein/CommentPageActivity.java
@@ -1,10 +1,20 @@
 package cs.ualberta.ca.tunein;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class CommentPageActivity extends Activity {
 
+	//reply view adapter
+	private ReplyViewAdapter viewAdapter;
+	//comment passed through intent when clicking on a view comment button
+	private Comment aComment;
+	//reply list
+	private ArrayList<Comment> replies;
+	
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
