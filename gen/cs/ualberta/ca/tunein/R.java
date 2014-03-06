898e8a43687af492a306e8a0039f965682bee7f7
kchow3 <kchow3@ualberta.ca>
2014-03-06 09:03:43 -0700

    Added xml page for comment view. A page that has a comment and its replies.

--- gen/cs/ualberta/ca/tunein/R.java
+++ gen/cs/ualberta/ca/tunein/R.java
@@ -57,10 +57,11 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int comment_view_row=0x7f030001;
-        public static final int create_comment_view=0x7f030002;
-        public static final int reply_view_row=0x7f030003;
-        public static final int topic_list_view=0x7f030004;
+        public static final int comment_view=0x7f030001;
+        public static final int comment_view_row=0x7f030002;
+        public static final int create_comment_view=0x7f030003;
+        public static final int reply_view_row=0x7f030004;
+        public static final int topic_list_view=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
