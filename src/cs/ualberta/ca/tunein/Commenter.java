bbd26d153d33a1e5036c15a16fdb250bb99322d9
kchow3 <kchow3@ualberta.ca>
2014-03-07 09:06:37 -0700

    Added javadoc comments to the commenter class.

--- src/cs/ualberta/ca/tunein/Commenter.java
+++ src/cs/ualberta/ca/tunein/Commenter.java
@@ -1,12 +1,26 @@
 package cs.ualberta.ca.tunein;
 
+/**
+ * Model
+ * Commenter Class:
+ * This is a model class for the user that creates a comment.
+ * This class is not complete yet. The only attributes right now
+ * is a name for the commenter. 
+ * TODO:add a unique property to the
+ * commenter class that is linked to their phone so that only they
+ * can change thier comments.
+ */
 public class Commenter {
 
 	private String name;
 	
+	/**
+	 * Constructor that constructs a commenter.
+	 * @param name The username of the commenter.
+	 */
 	public Commenter(String name) 
 	{
-		this.name = name;
+		this.name = "Anonymous";
 	}
 
 	public String getName() {
