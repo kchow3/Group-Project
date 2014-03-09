dfec76ccd8e7257a6756950c87b5aa27943589e6
kchow3 <kchow3@ualberta.ca>
2014-03-09 14:02:40 -0600

    Finished up code for group view in reply view adapter. Now need to implement click listner in reply view adapter.

--- src/cs/ualberta/ca/tunein/ReplyViewAdapter.java
+++ src/cs/ualberta/ca/tunein/ReplyViewAdapter.java
@@ -95,11 +95,13 @@ public class ReplyViewAdapter extends BaseExpandableListAdapter{
 		holder.textViewReplyDate.setText(replies.get(groupPosition).getReplies().get(childPosition).dateToString());
 		
 		//set onclick listeners for buttons and the tag for position
-		holder.buttonRowReply.setOnClickListener(replyBtnClick);
+		holder.buttonRowReply.setOnClickListener(replyChildBtnClick);
 		holder.buttonRowReply.setTag(childPosition);
-		holder.buttonReplyView.setOnClickListener(viewBtnClick);
+		holder.buttonReplyView.setOnClickListener(viewChildBtnClick);
 		holder.buttonReplyView.setTag(childPosition);
 		
+		//set the holder
+        rowView.setTag(holder);
 		
 		return rowView;
 	}
@@ -107,50 +109,78 @@ public class ReplyViewAdapter extends BaseExpandableListAdapter{
 
 	@Override
 	public int getChildrenCount(int groupPosition) {
-		// TODO Auto-generated method stub
-		return 0;
+		ArrayList<Comment> list = replies.get(groupPosition).getReplies();
+		return list.size();
 	}
 
 
 	@Override
 	public Object getGroup(int groupPosition) {
-		// TODO Auto-generated method stub
-		return null;
+		return replies.get(groupPosition);
 	}
 
 
 	@Override
 	public int getGroupCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return replies.size();
 	}
 
 
 	@Override
 	public long getGroupId(int groupPosition) {
-		// TODO Auto-generated method stub
-		return 0;
+		return groupPosition;
 	}
 
 
 	@Override
 	public View getGroupView(int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		//declare a new holder
+		holder = new ViewHolder();
+				
+		//create inflater
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				
+		//get rowView from inflater
+		View rowView = null;
+		rowView = inflater.inflate(R.layout.comment_view_row, parent, false);
+		
+		//set all the elements in the custom comment row
+		holder.textViewReply = (TextView) rowView.findViewById(R.id.textViewReply);
+		holder.textViewReplyRowCount = (TextView) rowView.findViewById(R.id.textViewReplyRowCount);
+		holder.textViewReplyUser = (TextView) rowView.findViewById(R.id.textViewReplyUser);
+		holder.textViewReplyDate = (TextView) rowView.findViewById(R.id.textViewReplyDate);
+		holder.buttonRowReply = (Button) rowView.findViewById(R.id.buttonRowReply);
+		holder.buttonReplyView = (Button) rowView.findViewById(R.id.buttonReplyView);
+		
+		//set text of textviews
+		holder.textViewReply.setText(replies.get(groupPosition).getComment());
+		holder.textViewReplyRowCount.setText("Replies: " + Integer.toString(replies.get(groupPosition).getReplyCount()));
+		holder.textViewReplyUser.setText(replies.get(groupPosition).getCommenter().getName());
+		holder.textViewReplyDate.setText(replies.get(groupPosition).dateToString());
+		
+		//set onclick listeners for buttons and the tag for position
+		holder.buttonRowReply.setOnClickListener(replyParentBtnClick);
+		holder.buttonRowReply.setTag(groupPosition);
+		holder.buttonReplyView.setOnClickListener(viewParentBtnClick);
+		holder.buttonReplyView.setTag(groupPosition);
+		
+		//set the holder
+        rowView.setTag(holder);
+		
+		return rowView;
 	}
 
 
 	@Override
 	public boolean hasStableIds() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 
 	@Override
 	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
@@ -159,7 +189,7 @@ public class ReplyViewAdapter extends BaseExpandableListAdapter{
 	 * This click listener will send user to CommentViewPage of the comment
 	 * that they clicked view on.
 	 */
-	private OnClickListener viewBtnClick = new OnClickListener() 
+	private OnClickListener viewChildBtnClick = new OnClickListener() 
 	{
 	    public void onClick(View v)
 	    {
@@ -170,7 +200,29 @@ public class ReplyViewAdapter extends BaseExpandableListAdapter{
 	 * This click listener will send user a comment creation dialog box
 	 * so that they can reply to a comment that they clicked reply on.
 	 */
-	private OnClickListener replyBtnClick = new OnClickListener() 
+	private OnClickListener replyChildBtnClick = new OnClickListener() 
+	{
+	    public void onClick(View v)
+	    {
+	    }
+	};
+	
+	/**
+	 * This click listener will send user to CommentViewPage of the comment
+	 * that they clicked view on.
+	 */
+	private OnClickListener viewParentBtnClick = new OnClickListener() 
+	{
+	    public void onClick(View v)
+	    {
+	    }
+	};
+	
+	/**
+	 * This click listener will send user a comment creation dialog box
+	 * so that they can reply to a comment that they clicked reply on.
+	 */
+	private OnClickListener replyParentBtnClick = new OnClickListener() 
 	{
 	    public void onClick(View v)
 	    {
