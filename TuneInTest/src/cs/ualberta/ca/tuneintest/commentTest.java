541494b84b91904dcdd874550ee15ab6ee2662c9
bwylie1 <bwylie@ualberta.ca>
2014-03-12 16:47:58 -0600

    Added test framework for Comment class

--- TuneInTest/src/cs/ualberta/ca/tuneintest/commentTest.java
+++ TuneInTest/src/cs/ualberta/ca/tuneintest/commentTest.java
@@ -1,15 +1,17 @@
 package cs.ualberta.ca.tuneintest;
 
-import junit.framework.TestCase;
+import java.util.Date;
 
-public class commentTest extends TestCase {
-//Need to add the following tests: commenter's name displays properly
-//comment title displays properly
-//replies display properly
-//images can be seen and added to comments
-//date information is displayed properly
-//geo location info can be seen and is accurate
-//fav and save info can be seen
-//reply count is displayed properly
+import android.test.ActivityInstrumentationTestCase2;
+import cs.ualberta.ca.tunein.MainActivity;
+import cs.ualberta.ca.tunein.Comment;
+
+public class commentTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	
+	public commentTest() {
+		super(MainActivity.class);
+	}
+	
+//Need to add tests for all properties of the Comment class
 
 }
