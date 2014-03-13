f965c318cb4ff7b8ed159e9875c482d6006795d7
bwylie1 <bwylie@ualberta.ca>
2014-03-13 15:58:53 -0600

    Edited testcase for Comment class

--- TuneInTest/src/cs/ualberta/ca/tuneintest/commentTest.java
+++ TuneInTest/src/cs/ualberta/ca/tuneintest/commentTest.java
@@ -15,24 +15,13 @@ public class commentTest extends ActivityInstrumentationTestCase2<MainActivity> 
 	}
 	
 	public void testEquals() {
-		Commenter commenter = new Commenter("Anonymous", "ABC");
+		Commenter commenter = new Commenter("myName", "ID");
 		GeoLocation loc = new GeoLocation(0, 0);
-		
+	
 		Comment comment0 = new Comment(commenter , "Sample Title", "sample comment", loc);
 		Comment comment1 = new Comment(commenter, "Other Title", "other content", loc);
 		
 		assertFalse("different comments are not equal", comment0.equals(comment1));
 	}
 	
-//Need to add tests for all properties of the Comment class
-//Need to add the following tests: commenter's name displays properly
-//comment title displays properly
-//replies display properly
-//images can be seen and added to comments
-//date information is displayed properly
-//geo location info can be seen and is accurate
-//fav and save info can be seen
-//reply count is displayed properly
-
-
 }
