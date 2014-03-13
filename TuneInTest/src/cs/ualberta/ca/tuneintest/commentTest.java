24325978d4bd1dcd80847c4f1872076c6f190ffb
bwylie1 <bwylie@ualberta.ca>
2014-03-13 13:46:46 -0600

    Made testcase for Comment class. Not complete yet.

--- TuneInTest/src/cs/ualberta/ca/tuneintest/commentTest.java
+++ TuneInTest/src/cs/ualberta/ca/tuneintest/commentTest.java
@@ -5,11 +5,23 @@ import java.util.Date;
 import android.test.ActivityInstrumentationTestCase2;
 import cs.ualberta.ca.tunein.MainActivity;
 import cs.ualberta.ca.tunein.Comment;
+import cs.ualberta.ca.tunein.Commenter;
+import cs.ualberta.ca.tunein.GeoLocation;
 
 public class commentTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	
 	public commentTest() {
 		super(MainActivity.class);
+	}
+	
+	public void TestEquals() {
+		Commenter commenter = new Commenter("Anonymous", "ABC");
+		GeoLocation loc = new GeoLocation(0, 0);
+		
+		Comment comment0 = new Comment(commenter , "Sample Title", "sample comment", loc);
+		Comment comment1 = new Comment(commenter, "Other Title", "other content", loc);
+		
+		assertTrue("different comments are not equal", comment0.equals(comment1));
 	}
 	
 //Need to add tests for all properties of the Comment class
