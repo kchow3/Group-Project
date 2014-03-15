15bd736801e8259547bbf45971bffea92656538c
bwylie1 <bwylie@ualberta.ca>
2014-03-15 16:37:39 -0600

    Added test framework for ElasticSearch classes

---
+++ TuneInTest/src/cs/ualberta/ca/tuneintest/bitmapJsonConverterTest.java
@@ -0,0 +1,15 @@
+package cs.ualberta.ca.tuneintest;
+
+import android.test.ActivityInstrumentationTestCase2;
+import cs.ualberta.ca.tunein.MainActivity;
+import cs.ualberta.ca.tunein.network.BitmapJsonConverter;
+
+public class bitmapJsonConverterTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	
+	public bitmapJsonConverterTest() {
+		super(MainActivity.class);
+	}
+	
+//Need to add tests for all properties of the BitmapJsonConverter class
+
+}
---
+++ TuneInTest/src/cs/ualberta/ca/tuneintest/elasticSearchOperationsTest.java
@@ -0,0 +1,15 @@
+package cs.ualberta.ca.tuneintest;
+
+import android.test.ActivityInstrumentationTestCase2;
+import cs.ualberta.ca.tunein.MainActivity;
+import cs.ualberta.ca.tunein.network.ElasticSearchOperations;
+
+public class elasticSearchOperationsTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	
+	public elasticSearchOperationsTest() {
+		super(MainActivity.class);
+	}
+	
+//Need to add tests for all properties of the ElasticSearchOperations class
+
+}
