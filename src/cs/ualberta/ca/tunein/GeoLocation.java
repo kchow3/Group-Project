3de26e05f926140a516bc8b472bf60fe049ee3db
kchow3 <kchow3@ualberta.ca>
2014-03-15 23:58:59 -0600

    Added GeoLocation Controller for loading in gps coords.

--- src/cs/ualberta/ca/tunein/GeoLocation.java
+++ src/cs/ualberta/ca/tunein/GeoLocation.java
@@ -32,7 +32,7 @@ public class GeoLocation implements Serializable{
 		return longitude;
 	}
 
-	public void setLongitude(int longitude) {
+	public void setLongitude(double longitude) {
 		this.longitude = longitude;
 	}
 
@@ -40,7 +40,7 @@ public class GeoLocation implements Serializable{
 		return latitude;
 	}
 
-	public void setLatitude(int latitude) {
+	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
 
