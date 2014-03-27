551fd44117df5a8e78f6efaf3e1db503580801b5
kchow3 <kchow3@ualberta.ca>
2014-03-26 21:30:52 -0600

    Finshed favorites activity, need to test.

--- src/cs/ualberta/ca/tunein/FavoriteActivity.java
+++ src/cs/ualberta/ca/tunein/FavoriteActivity.java
@@ -2,19 +2,25 @@ package cs.ualberta.ca.tunein;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class FavoriteActivity extends Activity {
 
 	private FavoriteController favoriteController;
 	//comment view adapter
 	private CommentViewAdapter viewAdapter;
+	private Favorites favs;
 	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.favorites_view);
+	    favs = Favorites.getInstance();
 	    favoriteController = new FavoriteController();
+	    viewAdapter = new CommentViewAdapter(FavoriteActivity.this, favs.favorites);
+	    ListView listview = (ListView) findViewById(R.id.listViewFavorites);
+		listview.setAdapter(viewAdapter);
 	}
 	
 	@Override
@@ -22,6 +28,7 @@ public class FavoriteActivity extends Activity {
 	{
 		super.onResume();
 		favoriteController.loadFav(getApplicationContext());
+		viewAdapter.updateThreadView(favs.favorites);
 	}
 	
 
