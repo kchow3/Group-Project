1d8f723e7d4fa552448e55cb6cc0e76a3e175a19
kchow3 <kchow3@ualberta.ca>
2014-03-30 21:11:20 -0600

    Implemented half the methods for cache controller.

--- src/cs/ualberta/ca/tunein/CacheController.java
+++ src/cs/ualberta/ca/tunein/CacheController.java
@@ -1,10 +1,23 @@
 package cs.ualberta.ca.tunein;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import cs.ualberta.ca.tunein.network.BitmapJsonConverter;
 
@@ -18,7 +31,7 @@ import cs.ualberta.ca.tunein.network.BitmapJsonConverter;
  */
 public class CacheController {
 
-	public final static String FAV_FILE = "cache.sav";
+	public final static String SAVE_FILE = "cache.sav";
 
 	private Cache saves;
 	private Gson GSON;
@@ -35,12 +48,34 @@ public class CacheController {
 	
 	public void addToCache(Context cntxt, Comment comment)
 	{
-		
+		if(!(saves.cacheIDs.contains(comment.getElasticID())))
+		{
+
+			CharSequence text = "Saved!";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(cntxt, text, duration);
+			toast.show();
+		}
+		else
+		{
+			CharSequence text = "Already Saved.";
+			int duration = Toast.LENGTH_SHORT;
+
+			Toast toast = Toast.makeText(cntxt, text, duration);
+			toast.show();
+		}
 	}
 	
 	public void removeFromCache(Context cntxt, Comment comment)
 	{
-		
+		if((saves.cacheIDs.contains(comment.getElasticID())))
+		{
+			
+			CharSequence text = "Removed Saved Comment";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(cntxt, text, duration);
+			toast.show();
+		}
 	}
 	
 	public boolean inCache(Comment comment)
@@ -57,12 +92,51 @@ public class CacheController {
 	
 	private void saveCache(Context cntxt)
 	{
-		
+		Type cacheType = new TypeToken<Cache>(){}.getType();
+		String jsonString = GSON.toJson(saves, cacheType);
+        try {
+        	OutputStreamWriter outputStreamWriter = new OutputStreamWriter(cntxt.openFileOutput(SAVE_FILE, Context.MODE_PRIVATE));
+			outputStreamWriter.write(jsonString);
+	        outputStreamWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void loadCache(Context cntxt)
 	{
-		
+		File file = cntxt.getFileStreamPath(SAVE_FILE);
+		saves.cacheIDs = new ArrayList<String>();
+		saves.cacheList = new ArrayList<Comment>();
+		if(file.exists())
+		{
+			String jsonString = "";
+	
+		    try {
+		        InputStream inputStream = cntxt.openFileInput(SAVE_FILE);
+	
+		        if ( inputStream != null ) {
+		            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		            String receiveString = "";
+		            StringBuilder stringBuilder = new StringBuilder();
+	
+		            while ( (receiveString = bufferedReader.readLine()) != null ) {
+		                stringBuilder.append(receiveString);
+		            }
+	
+		            inputStream.close();
+		            jsonString = stringBuilder.toString();
+		        }
+		        Type cacheType = new TypeToken<Favorites>(){}.getType();
+		        saves.setInstance((Cache) GSON.fromJson(jsonString, cacheType));
+		    }
+		    catch (FileNotFoundException e) {
+		        Log.e("FAV:", "File not found: " + e.toString());
+		    } catch (IOException e) {
+		        Log.e("FAV:", "Can not read file: " + e.toString());
+		    }
+		}
 	}
 	
 	/**
