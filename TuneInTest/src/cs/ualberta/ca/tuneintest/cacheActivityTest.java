74bbd136491cb3c80ed2e706fcc3089808996fbf
bwylie1 <bwylie@ualberta.ca>
2014-04-06 14:13:29 -0600

    Added cacheActivityTest skeleton

---
+++ TuneInTest/src/cs/ualberta/ca/tuneintest/cacheActivityTest.java
@@ -0,0 +1,15 @@
+package cs.ualberta.ca.tuneintest;
+
+import android.test.ActivityInstrumentationTestCase2;
+import cs.ualberta.ca.tunein.MainActivity;
+import cs.ualberta.ca.tunein.CacheActivity;
+
+public class cacheActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	
+	public cacheActivityTest() {
+		super(MainActivity.class);
+	}
+	
+//Need to add tests for all properties of the CacheActivity class
+
+}
